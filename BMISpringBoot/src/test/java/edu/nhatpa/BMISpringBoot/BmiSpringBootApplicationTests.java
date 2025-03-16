8fb8e599c82bd3dde8812eec9260dfd131ae3fa2
PhanAnhNh <anhnhat1322004@gmail.com>
2025-03-16 17:41:50 +0700

    BMISpringBoot1

---
+++ BMISpringBoot/src/test/java/edu/nhatpa/BMISpringBoot/BmiSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.nhatpa.BMISpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmiSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
