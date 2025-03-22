7639cfc96b2831fa1fdd05e545df6210fa7758e4
PhanAnhNh <anhnhat1322004@gmail.com>
2025-03-23 00:39:13 +0700

    Layout

---
+++ ViduFragmet/src/main/java/edu/nhatpa/ViduFragmet/ViduFragmetApplication.java
@@ -0,0 +1,13 @@
+package edu.nhatpa.ViduFragmet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViduFragmetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViduFragmetApplication.class, args);
+	}
+
+}
---
+++ ViduFragmet/src/test/java/edu/nhatpa/ViduFragmet/ViduFragmetApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.nhatpa.ViduFragmet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViduFragmetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
