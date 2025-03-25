57df4d48a51a043bd76940c1bca48801926e7e2d
PhanAnhNh <anhnhat1322004@gmail.com>
2025-03-25 09:19:51 +0700

    Ontap

---
+++ OnTapGK/src/test/java/edu/nhatpa/OnTapGiuaKi/OnTapGkApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.nhatpa.OnTapGiuaKi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnTapGkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
